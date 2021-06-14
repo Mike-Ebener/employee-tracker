1a523821ff3cb0031dc194bc724e0096e0643d88
ME <mebener44@gmail.com>
2021-06-14 10:52:14 -0500

    working npm and tables in mysql

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,25 @@ DROP TABLE IF EXISTS employees;
 
 
 CREATE TABLE departments (
-    id INTEGER NOT NULL,
-    d_name VARCHAR(30) NOT NULL,
-)
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    d_name VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL, 
-)
+    department_id INTEGER NOT NULL
+);
 
 CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL,
+  role_id INTEGER,
   manager_id INTEGER,
---   CONSTRAINT fk_role
---     FOREIGN KEY (role_id)
---     REFERENCES roles(id)
---     ON DELETE SET NULL
+  CONSTRAINT fk_role
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
 );
