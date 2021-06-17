32b124b380e6e759d9b47fd01b67c362df0728fc
ME <mebener44@gmail.com>
2021-06-16 21:13:06 -0500

    working db

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,18 @@ DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    d_name VARCHAR(30) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL
+    department_id INTEGER,
+    CONSTRAINT fk_department
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -20,7 +24,7 @@ CREATE TABLE employees (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
-  manager_id INTEGER,
+    manager_id INTEGER,
   CONSTRAINT fk_role
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,16 @@
 
-INSERT INTO departments (id, d_name)
+INSERT INTO departments (id, name)
 VALUES
-    (1, Business),
-    (2, Design),
-    (3, Product),
-    (4, Engineering),
-    (5, Analytics),
-    (6, Marketing),
-    (7, Legal),
-    (8, Finance),
-    (9, People),
-    (10, Operations);
+    (1, 'Business'),
+    (2, 'Design'),
+    (3, 'Product'),
+    (4, 'Engineering'),
+    (5, 'Analytics');
+    -- (6, 'Marketing'),
+    -- (7, 'Legal'),
+    -- (8, 'Finance'),
+    -- (9, 'People'),
+    -- (10, 'Operations');
 
 INSERT INTO roles (id, title, salary, department_id)
 VALUES
@@ -20,12 +20,12 @@ VALUES
     (4, 'UX Designer', 85000, 2);
 
 
-INSERT INTO employees (id, first_name, last_name, role_id, manaager_id)
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
 VALUES
     (1, 'Mike', 'Ebener', 2, 2),
-    (2, 'Blaize', 'Johnston',2, 2),
-    (3, 'Raj', 'Patel',1, 3),
-    (4, 'Wendy', 'Narahali',1 ,4),
+    (2, 'Blaize', 'Johnston', 2, 2),
+    (3, 'Raj', 'Patel', 1, 3),
+    (4, 'Wendy', 'Narahali', 1 ,4),
     (5, 'Blake', 'Worker', 3, 2),
     (6, 'Adam', 'Demamp', 4, 4),
     (7, 'Carl', 'Sketch', 3, 3),
