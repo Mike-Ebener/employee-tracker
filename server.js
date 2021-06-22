a8e3dd378ec8b23bebe23587e9d48f27e3ac83c1
ME <mebener44@gmail.com>
2021-06-21 20:03:31 -0500

    routes working from Insomnia

--- server.js
+++ server.js
@@ -17,10 +17,128 @@ const db = mysql.createConnection(
       user: 'root',
       // Your MySQL password
       password: '4Horsemen#',
-      database: 'election'
+      database: 'staff'
     },
-    console.log('Connected to the election database.')
+    console.log('Connected to the staff database.')
   );
+
+ // Get all employees
+app.get('/api/employees', (req, res) => {
+    const sql = `SELECT * FROM employees`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+   // Get all roles
+app.get('/api/roles', (req, res) => {
+    const sql = `SELECT * FROM roles`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+     // Get all departments
+app.get('/api/departments', (req, res) => {
+    const sql = `SELECT * FROM departments`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  // Create a employee
+app.post('/api/employee', ({ body }, res) => {
+    
+    const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+    const params = [
+      body.first_name,
+      body.last_name,
+      body.role_id,
+      body.manager_id
+    ];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body,
+        changes: result.affectedRows
+      });
+    });
+  });
+
+    // Create a role (id, title, salary, department_id)
+app.post('/api/role', ({ body }, res) => {
+    
+    const sql = `INSERT INTO roles (id, title, salary, department_id) VALUES (?,?,?,?)`;
+    const params = [
+      body.id,
+      body.title,
+      body.salary,
+      body.department_id
+    ];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body,
+        changes: result.affectedRows
+      });
+    });
+  });
+
+      // Create a department (id, name)
+app.post('/api/department', ({ body }, res) => {
+    
+    const sql = `INSERT INTO departments (id, name) VALUES (?,?)`;
+    const params = [
+      body.id,
+      body.name
+    ];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body,
+        changes: result.affectedRows
+      });
+    });
+  });
 
 
 app.use((req, res) => {
